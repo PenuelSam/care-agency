fef696a506f8d74d1821204d528e611b6d26526a
PenuelSam <penuelsamuel54@gmail.com>
2023-05-24 19:10:11 +0100

    changed font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
         <div className='flex flex-col  justify-between items-center'>
           <div className='flex lg:flex-col sm:gap-[100px]  gap-[200px]'>
           <div className='flex gap-5'>
-            <LocationOnOutlinedIcon sx={{fontSize: "40px", color: "#fff"}}/>
+            <LocationOnOutlinedIcon sx={{fontSize: "30px", color: "#fff"}}/>
             <div>
-              <h1 className='text-[30px] sm:text-[30px] lg:text-[40px] font-bold text-white'>Address</h1>
-              <p className='text-[20px] sm:text-[20px] lg:text-[30px] text-white font-bold w-52'>Canalside House Office 1 383 Ladbroke Grove, London W10 5AA</p>
+              <h1 className='text-[20px] sm:text-[20px] lg:text-[30px] font-bold text-white'>Address</h1>
+              <p className='text-[15px] sm:text-[15px] lg:text-[20px] text-white font-bold w-52'>Canalside House Office 1 383 Ladbroke Grove, London W10 5AA</p>
             </div>
         </div>
         <div className='flex gap-5'>
